8cc1a3e8657abc7ec0388ed33b84a64f7c1bfe9c
Samin1615088 <samin.shafkat@gmail.com>
2020-09-26 00:15:57 +0600

    Repository is added as NoteRepository and in NoteDatabase we populate dummy data (previous one has logical error) this one is good

--- app/src/main/java/com/example/practiceandroidarchitecturetutorialone/NoteRepository.java
+++ app/src/main/java/com/example/practiceandroidarchitecturetutorialone/NoteRepository.java
@@ -17,27 +17,33 @@ public class NoteRepository {
         allNotes = noteDao.getAllNotes();
     }
 
-    public void insert(Note note){
+    public void insert(Note note) {
         new InsertNoteAsyncTask(noteDao).execute(note);
     }
 
-    public void update(Note note){
+    public void update(Note note) {
         new UpdateNoteAsyncTask(noteDao).execute(note);
     }
 
-    public void delete(Note note){
-        new DeleteAllNoteAsyncTask(noteDao).execute(note);
+    public void delete(Note note) {
+        new DeleteNoteAsyncTask(noteDao).execute(note);
     }
 
-    public LiveData<List<Note>> getAllNotes(){
+    public void deleteAll(Note note) {
+        new DeleteAllNoteAsyncTask(noteDao).execute();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
         return this.allNotes;
     }
 
-    private static class InsertNoteAsyncTask extends AsyncTask<Note, Void, Void>{
+    private static class InsertNoteAsyncTask extends AsyncTask<Note, Void, Void> {
         private NoteDao noteDao;
-        private InsertNoteAsyncTask(NoteDao noteDao){
+
+        private InsertNoteAsyncTask(NoteDao noteDao) {
             this.noteDao = noteDao;
         }
+
         @Override
         protected Void doInBackground(Note... notes) {
             noteDao.insert(notes[0]);
@@ -45,11 +51,13 @@ public class NoteRepository {
         }
     }
 
-    private static class UpdateNoteAsyncTask extends AsyncTask<Note, Void, Void>{
+    private static class UpdateNoteAsyncTask extends AsyncTask<Note, Void, Void> {
         private NoteDao noteDao;
-        private UpdateNoteAsyncTask(NoteDao noteDao){
+
+        private UpdateNoteAsyncTask(NoteDao noteDao) {
             this.noteDao = noteDao;
         }
+
         @Override
         protected Void doInBackground(Note... notes) {
             noteDao.update(notes[0]);
@@ -57,15 +65,32 @@ public class NoteRepository {
         }
     }
 
-    private static class DeleteAllNoteAsyncTask extends AsyncTask<Note, Void, Void>{
+    private static class DeleteAllNoteAsyncTask extends AsyncTask<Void, Void, Void> {
         private NoteDao noteDao;
-        private DeleteAllNoteAsyncTask(NoteDao noteDao){
+
+        private DeleteAllNoteAsyncTask(NoteDao noteDao) {
             this.noteDao = noteDao;
         }
+
         @Override
-        protected Void doInBackground(Note... notes) {
+        protected Void doInBackground(Void... voids) {
             noteDao.deleteAll();
             return null;
         }
     }
+
+    private static class DeleteNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+        private NoteDao noteDao;
+        private DeleteNoteAsyncTask(NoteDao noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.delete(notes[0]);
+            return null;
+        }
+    }
+
+
 }
