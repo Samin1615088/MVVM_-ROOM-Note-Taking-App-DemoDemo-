c80b618178327c860f001f7217e8753120aba19a
Samin1615088 <samin.shafkat@gmail.com>
2020-09-30 20:21:24 +0600

    add note task is done completely

--- app/src/main/java/com/example/practiceandroidarchitecturetutorialone/AddNoteActivity.java
+++ app/src/main/java/com/example/practiceandroidarchitecturetutorialone/AddNoteActivity.java
@@ -1,14 +1,77 @@
 package com.example.practiceandroidarchitecturetutorialone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
 
 public class AddNoteActivity extends AppCompatActivity {
+    private EditText edTitle;
+    private EditText edDescription;
+    private NumberPicker nmPriority;
+
+    static final String EXTRA_TITLE = "com.example.action.TITLE_EXTRA";
+    static final String EXTRA_DESCRIPTION = "com.example.action.DESCRIPTION_EXTRA";
+    static final String EXTRA_PRIORITY = "com.example.action.PRIORITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+
+        edTitle =  findViewById(R.id.ed_title);
+        edDescription =  findViewById(R.id.ed_description);
+        nmPriority = findViewById(R.id.nm_priority);
+
+        nmPriority.setMaxValue(10);
+        nmPriority.setMinValue(1);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);     //need more attention to understand it
+        setTitle("Add Note");
+    }
+
+    private void saveNote(){
+        String title = edTitle.getText().toString();
+        String description = edDescription.getText().toString();
+        int priority = nmPriority.getValue();
+
+        if(title.trim().isEmpty() || description.trim().isEmpty()){
+            Toast.makeText(this,"Please insert a title and description", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent data = new Intent(this, MainActivity.class  );
+        data.putExtra(EXTRA_TITLE, title);
+        data.putExtra(EXTRA_DESCRIPTION, description);
+        data.putExtra(EXTRA_PRIORITY, priority);
+        setResult(RESULT_OK, data);
+        finish();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.save_note:
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/practiceandroidarchitecturetutorialone/MainActivity.java
+++ app/src/main/java/com/example/practiceandroidarchitecturetutorialone/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.practiceandroidarchitecturetutorialone;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -7,18 +8,51 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private NoteViewModel noteViewModel;
+    public static final int ADD_NOTE_REQUEST = 1;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK) {
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description = data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY, 1);
+
+            Note note = new Note(title,description, priority);
+            noteViewModel.insert(note);
+
+            Toast.makeText(MainActivity.this, "note saved", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(MainActivity.this, "note not saved", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton buttonAddNote = findViewById(R.id.bt_fab_add);
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), AddNoteActivity.class);
+                startActivityForResult(intent, ADD_NOTE_REQUEST);
+            }
+        });
 
         RecyclerView recyclerView = findViewById(R.id.recycle_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -36,5 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
 }
